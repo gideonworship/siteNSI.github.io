663a2c553d78a8a4a5aa0ee430a26516c040d178
gideonworship <82960520+gideonworship@users.noreply.github.com>
2021-04-25 00:40:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+﻿
+var prenom=prompt("Entrez votre prénom :");
+var date=Date();
+alert("Bonjour "+prenom+".\nNous sommes le :"+date+"\nATTENTION : LES E3C APPROCHENT!!\nVoici ce que tu dois savoir sur la matière NSI en première.");
